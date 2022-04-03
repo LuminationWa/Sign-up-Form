9379e7ffc7bf362b100e137565a7b46d55a87790
LuminationWa <edelmedina322@gmail.com>
2022-04-03 19:40:32 -0300

    Remade it

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function check(input) {
     if (input.value != document.getElementById('user_password').value) {
-        input.setCustomValidity('Password Must be Matching.');
+        input.setCustomValidity('The passwords must match');
     } else {
         // input is valid -- reset the error message
         input.setCustomValidity('');
