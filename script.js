623c59220c35fb76607d28a1a33d99a9d6cc2511
LuminationWa <edelmedina322@gmail.com>
2022-04-03 11:39:25 -0300

    Idk, will remake later

---
+++ script.js
@@ -0,0 +1,8 @@
+function check(input) {
+    if (input.value != document.getElementById('user_password').value) {
+        input.setCustomValidity('Password Must be Matching.');
+    } else {
+        // input is valid -- reset the error message
+        input.setCustomValidity('');
+    }
+}
